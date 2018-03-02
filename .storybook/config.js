a55c412f89b6ff709ed137c0ff8a271c5e924d86
Valentin Agachi <github-com@agachi.name>
2018-03-02 16:00:16 +0100

    Storybook screenshots addon :camera:

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,23 @@
-import { configure } from '@storybook/react';
+import { addDecorator, configure } from '@storybook/react';
+import {
+	initScreenshot,
+	setScreenshotOptions,
+	withScreenshot,
+} from 'storybook-chrome-screenshot';
+
+setScreenshotOptions({
+	viewport: {
+		width: 300,
+		height: 100,
+	},
+});
+addDecorator(initScreenshot());
+addDecorator(withScreenshot());
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
 function loadStories() {
-  req.keys().forEach((filename) => req(filename));
+	req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
